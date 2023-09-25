6cf939ed74bd4920e819e4a7b5b02501d92325ec
Vizeu <leovizeu@hotmail.com>
2023-09-25 18:03:18 -0300

    Mapeamento do usuario na classe Modelo.

--- src/main/java/br/com/agendamento/api/service/cadastro/CreateTaskService.java
+++ src/main/java/br/com/agendamento/api/service/cadastro/CreateTaskService.java
@@ -6,7 +6,6 @@ import br.com.agendamento.api.exceptions.InternalErrorException;
 import br.com.agendamento.api.exceptions.ValidacaoException;
 import br.com.agendamento.api.model.CreateTaskModel;
 import br.com.agendamento.api.model.Status;
-import br.com.agendamento.api.model.Usuario;
 import br.com.agendamento.api.repository.CreateTaskRepository;
 import br.com.agendamento.api.service.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
